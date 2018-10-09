b5c1e2f9d6e973a479e1f09968aa09c30bdc0a9a
geyouwen <690591423@qq.com>
2018-10-09 17:57:21 +0800

    修改介绍

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='applehttp2push',
-    version='0.0.1',
+    version='0.0.2',
     description='一个基于python2和http2的苹果推送SDK，基于yubang的修改。增加文件验证密码',
     author='geyouwen',
     author_email='690591423@qq.com',
