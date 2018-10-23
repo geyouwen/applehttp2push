b75f132e277656513774905a2c5872b5d4558432
geyouwen <690591423@qq.com>
2018-10-23 11:01:57 +0800

    区分推送标题和信息主体

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='applehttp2push',
-    version='0.0.3',
-    description='一个基于python2和http2的苹果推送SDK，基于yubang的修改。增加文件验证密码',
+    version='1.0.0',
+    description='一个基于python2和http2的苹果推送SDK，基于yubang的修改。增加文件验证密码，区分推送标题和主体，增加声音控制。',
     author='geyouwen',
     author_email='690591423@qq.com',
     url='https://github.com/geyouwen/applehttp2push',
